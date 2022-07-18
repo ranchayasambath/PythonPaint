6e5c6faeba02d1bbe1fd755b6fa0cb5e71a46f42
Sambath Ranchaya <sambath.ranchaya@e-legalinc.com>
2022-07-18 17:00:31 -0500

    python paint

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils import *
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Drawing Program")
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
+
